f677bcd5fa3c8058d4d445fe1c736e26b2836df7
wacdev <i@wac.tax>
2023-04-22 10:18:26 +0700

    ♨

--- api/Core/init/pg.sql
+++ api/Core/init/pg.sql
@@ -1,3 +1,28 @@
 CREATE EXTENSION IF NOT EXISTS moreint;
 
+UPDATE
+  pg_type
+SET oid = 100000
+WHERE typname = 'u64';
+
+UPDATE
+  pg_type
+SET oid = 100001
+WHERE typname = 'u32';
+
+UPDATE
+  pg_type
+SET oid = 100002
+WHERE typname = 'u16';
+
+UPDATE
+  pg_type
+SET oid = 100003
+WHERE typname = 'u8';
+
+UPDATE
+  pg_type
+SET oid = 100004
+WHERE typname = 'i8';
+
 CREATE EXTENSION IF NOT EXISTS md5hash;
