33bd912e341d70f2d657113da652300822dcfb0b
wacdev <i@wac.tax>
2023-09-13 01:47:10 +0800

    ♨

--- api/Sql/clientNew.js
+++ api/Sql/clientNew.js
@@ -26,11 +26,11 @@ export default (
 		"SELECT client_new($1,$2,$3,$4,$5,$6,$7,$8)",
 		client_id,
 		ip,
-		browser_name,
-		browser_ver,
-		os_name,
-		os_ver,
-		device_vendor,
-		device_model,
+		browser_name or '',
+		browser_ver or '',
+		os_name or '',
+		os_ver or '',
+		device_vendor or '',
+		device_model or '',
 	);
 };
